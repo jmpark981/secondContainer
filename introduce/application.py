1dff9f29d6da007f27f320b9326b75be2a6028f1
jmpark981 <joonmo95@gmail.com>
2023-03-01 06:39:48 +0000

    second commit

--- introduce/application.py
+++ introduce/application.py
@@ -31,7 +31,7 @@ class Usertbl(db.Model):
     college_of=db.Column(db.String(30), nullable=False)
     how_eat=db.Column(db.String(45), nullable=False)
 
-
+#멀티 체크박스 필드 생성
 class MultiCheckBoxField(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
@@ -46,9 +46,11 @@ class UserForm(FlaskForm):
     how_eat=MultiCheckBoxField("5. 식사해결방법", choices=[('기숙사 식당', '기숙사 식당'), ('외식', '외식'), ('기숙사 내', '기숙사 내')])
     submit=SubmitField("Submit")
 
+    
 @app.route("/")
 def hello():
     return render_template("index.html")
+
 
 @app.route("/servey", methods=["GET", "POST"])
 def servey():
@@ -57,6 +59,7 @@ def servey():
     sex=None
     college_of=None
     how_eat=None
+    error_msg=""
     form=UserForm(request.form)
     
     #Validate Form
@@ -66,18 +69,21 @@ def servey():
         existingEmail=Usertbl.query.filter_by(email=form.email.data).first()
         print(existingEmail)
         
+        #해당 이메일을 가진 유저가 존재한다면 (이메일 중복 방지) 아니면 DB에 추가
         if existingEmail is not None:
             form=UserForm(formdata=None)
-            return render_template("servey.html", email=email, age=age, sex=sex, college_of=college_of, how_eat=how_eat, form=form)
+            error_msg="중복되는 이메일"
+            return render_template("servey.html", error_msg=error_msg, email=email, age=age, sex=sex, college_of=college_of, how_eat=how_eat, form=form)
         else:
-            user=Usertbl(email=form.email.data, age=form.age.data, sex=form.sex.data, college_of=form.college_of.data, how_eat=form.how_eat.data)
+            user=Usertbl(email=form.email.data, age=form.age.data, sex=form.sex.data, college_of=form.college_of.data, how_eat="&".join(form.how_eat.data))
             db.session.add(user)
             db.session.commit()
             return redirect(url_for('qna', email=form.email.data))
-
+        
+        #Clear Form
         form=UserForm(formdata=None)
     
-    return render_template("servey.html", email=email, age=age, sex=sex, college_of=college_of, how_eat=how_eat, form=form)
+    return render_template("servey.html", error_msg=error_msg, email=email, age=age, sex=sex, college_of=college_of, how_eat=how_eat, form=form)
 
 @app.route("/result")
 def result():
@@ -85,14 +91,12 @@ def result():
 
 @app.route("/qna/<email>", methods=["GET", "POST"])
 def qna(email):
-    print(email)
-    #print(age, sex, college_of, how_eat)
-    return render_template("qna.html", email=email)
+    show_users=Usertbl.query.all()
+    return render_template("qna.html", email=email, show_users=show_users)
 
 @app.route("/creator")
 def creator():
     return render_template("creator.html")
-
 
 
 if __name__ == "__main__":
