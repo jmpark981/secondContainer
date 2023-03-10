607c9804701b4e461e68c9ce2c82ea0681c19d61
jmpark981 <joonmo95@gmail.com>
2023-03-10 15:43:49 +0000

    survey done

--- introduce/application.py
+++ introduce/application.py
@@ -8,11 +8,11 @@ from flask_sqlalchemy import SQLAlchemy
 import sys
 
 
-app = Flask(__name__)
-csrf = CSRFProtect(app)
+app = Flask(__name__)      # 서버 생성
+csrf = CSRFProtect(app)    # form이 제대로 전송되었는지 확인
 
 #데이터 베이스 추가
-app.config['SQLALCHEMY_DATABASE_URI']="mysql+pymysql://xxxxx:xxxxxx@localhost/users"
+app.config['SQLALCHEMY_DATABASE_URI']="mysql+pymysql://joonmo:os2window@localhost/users"
 
 #폼 비밀키 설정
 app.config['SECRET_KEY']="my super secret key"
@@ -21,16 +21,16 @@ app.config['SECRET_KEY']="my super secret key"
 #Initalize DB
 db=SQLAlchemy(app)
 
-#모델 생성
+#모델 생성 -> table 생성
 class Usertbl(db.Model):
     __table_name__ = 'usertbl'
     
     email=db.Column(db.String(30), primary_key=True)
-    age=db.Column(db.Integer, nullable=False)
     sex=db.Column(db.String(5), nullable=False)
+    age=db.Column(db.Integer, nullable=False)
+    dorm_select=db.Column(db.String(10), nullable=False)
+    
     college_of=db.Column(db.String(30), nullable=False)
-    newbie_in=db.Column(db.String(5), nullable=False)
-    diff_college_of=db.Column(db.String(5), nullable=False)
     personality=db.Column(db.String(5), nullable=False)
     weekend_stay=db.Column(db.String(5), nullable=False)
     weekday_stay=db.Column(db.String(30), nullable=False)
@@ -39,18 +39,12 @@ class Usertbl(db.Model):
     how_eat=db.Column(db.String(45), nullable=False)
     how_eat_in=db.Column(db.String(5), nullable=False)
     wake_up=db.Column(db.String(10), nullable=False)
-    stay_overnight_test=db.Column(db.String(10), nullable=False)
     sleep=db.Column(db.String(5), nullable=False)
+    sleep_sensitive=db.Column(db.String(10), nullable=False)
     sleep_habit=db.Column(db.String(5), nullable=False)
     clean_period=db.Column(db.String(10), nullable=False)
-    shower_time=db.Column(db.String(10), nullable=False)
     shower_timezone=db.Column(db.String(5), nullable=False)
-    material_share=db.Column(db.String(5), nullable=False)
-    bug_catch=db.Column(db.String(5), nullable=False)
-    phone_chat_in=db.Column(db.String(5), nullable=False)
-    phone_chat_time=db.Column(db.String(10), nullable=False)
-    age_range=db.Column(db.String(10), nullable=False)
-
+    
 
 #멀티 체크박스 필드 생성
 class MultiCheckBoxField(SelectMultipleField):
@@ -59,43 +53,39 @@ class MultiCheckBoxField(SelectMultipleField):
 
 
 #폼 클래스 만들기
-class UserForm(FlaskForm):
+class BaseForm(FlaskForm):
+    #설문지
+    personality=RadioField("6. 성격 유형", choices=[('E','외향적'),('I','내향적')], validators=[InputRequired()])
+    weekend_stay=RadioField("7. 주말 거주 유형", choices=[('주로 기숙사내','주로 기숙사내'),('주로 기숙사 밖','주로 기숙사 밖')], validators=[InputRequired()])
+    weekday_stay=RadioField("8. 평일 거주 유형", choices=[('아침', '아침'), ('점심', '점심'), ('저녁', '저녁')])
+    smoke=RadioField("9. 흡연 유무", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
+    alcohol=RadioField("10. 음주 정도", choices=[('자주', '자주'), ('보통', '보통'), ('드물게', '드물게')])
+    how_eat=RadioField("11. 식사 해결 장소", choices=[('기숙사 내', '기숙사 내'), ('기숙사 밖', '기숙사 밖'), ('기숙사 식당', '기숙사 식당')])
+    how_eat_in=RadioField("12. 배달음식 같이 먹기 희망 여부", choices=[('Y','매우 희망'),('N','비희망')], validators=[InputRequired()])
+    wake_up=RadioField("13. 평균 기상 시간", choices=[('6시~8시','6시~8시'),('8시~10시','8시~10시'),('10시~12시','10시~12시'), ('12시이후','12시 이후')], validators=[InputRequired()])
+    sleep=RadioField("14. 평균 취침 시간", choices=[('10시~12시','10시~12시'),('12시~2시','12시~2시'),('2시 이후','새벽')], validators=[InputRequired()])
+    sleep_sensitive=RadioField("15. 취침시 예민도", choices=[('예민','예민'),('둔감','둔감')], validators=[InputRequired()])
+    sleep_habit=RadioField("16. 잠버릇 유무", choices=[('있다','그렇다'), ('보통', '보통'), ('없다','없다')], validators=[InputRequired()])
+    clean_period=RadioField("17. 일주일동안 청소 빈도", choices=[('자주(5회~7회)','자주(5회~7회)'),('보통(2회~4회)','보통(2회~4회)'),('드물게(1회~0회)','드물게(1회~0회)')], validators=[InputRequired()])
+    shower_timezone=RadioField("18. 샤워 시간대", choices=[('아침','아침'),('저녁','저녁')], validators=[InputRequired()])
+    
+    submit=SubmitField("다음 페이지로")
+
+class UserForm(BaseForm):
+    #유저 정보
     email=StringField("1.이메일", validators=[InputRequired()])
     age=IntegerField("2. 나이", validators=[InputRequired()])
     sex=RadioField("3. 성별", choices=[('M','남자'),('W','여자')], validators=[InputRequired()])
-    college_of=SelectField("4. 단과대", choices=[('경영대', '경영대'), ('사과대', '사과대'), ('예디대', '예디대'), ('공과대', '공과대'), ('융기원', '융기원')], validators=[InputRequired()])
-    newbie_in=RadioField("5. 신규입주생", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
+    dorm_select=RadioField("4. 거주 홀 정보", choices=[('레이크', '레이크홀'), ('비레이크', '비레이크홀')])
+    college_of=SelectField("5. 단과대", choices=[('문과대', '문과대'), ('이과대', '이과대'), ('건축대', '건축대'), ('공과대', '공과대'), ('사과대', '사과대'), 
+                                              ('경영대', '경영대'), ('부동산대', '부동산대'), ('융기원', '융기원'), ('생명과학대', '생명과학대'), ('수의대', '수의대'),
+                                              ('예디대', '예디대'), ('사범대', '사범대'), ('그외', '그외')], validators=[InputRequired()])
+    #1: 문과대	2: 이과대	3. 건축대	4. 공과대	5. 사과대	6. 경영대	7. 부동산	8. 융기원	9. 생명과학대 10. 수의대 11. 예디대 12. 사범대 13. 그외
+    
+class MateForm(BaseForm):
+    age_range=MultiCheckBoxField("20. 원하는 룸메 나이대", choices=[('또래', '두 살 차이 내'), ('또래 이상', '두 살 차이 이상')])
     diff_college_of=RadioField("6. 소속이 다른 사람 원하는 유무", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
-    personality=RadioField("7. 성격", choices=[('E','외향적'),('I','내향적')], validators=[InputRequired()])
-    
-    weekend_stay=RadioField("8-1. 주말 거주 유무", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
-    weekday_stay=MultiCheckBoxField("8-2. 평일 거주시간", choices=[('아침', '아침'), ('점심', '점심'), ('저녁', '저녁')])
-    
-    smoke=RadioField("9. 흡연 유무", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
-    alcohol=RadioField("10. 술 마시는 정도", choices=[('자주', '자주'), ('보통', '보통'), ('드물게', '드물게')])
-    how_eat=MultiCheckBoxField("11-1. 식사 해결 방법", choices=[('기숙사 식당', '기숙사 식당'), ('외식', '외식'), ('기숙사 안', '기숙사 안')])
-    how_eat_in=RadioField("11-2. 기숙사 안 같이 먹기 유무", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
-    
-    wake_up=RadioField("12. 기상 시간", choices=[('일찍','6시~8시'),('보통','8시~10시'),('늦게','12시 이후')], validators=[InputRequired()])
-    
-    sleep=RadioField("13-1. 취침 시간", choices=[('일찍','10시~12시'),('보통','12시~2시'),('늦게','새벽')], validators=[InputRequired()])
-    stay_overnight_test=RadioField("13-2. 시험 기간 밤샘 유무", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
-    
-    sleep_habit=RadioField("14. 잠버릇 허용 유무", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
-    clean_period=RadioField("15. 일주일동안 청소 빈도", choices=[('자주','자주(5~7)'),('보통','보통(5~7)'),('드물게','드물게(5~7)')], validators=[InputRequired()])
-    
-    shower_time=RadioField("16-1. 샤워 시간", choices=[('짧게','0~20분'),('길게','20분 over')], validators=[InputRequired()])
-    shower_timezone=RadioField("16-2. 샤워 시간대", choices=[('아침','아침'),('저녁','저녁')], validators=[InputRequired()])
-    
-    material_share=RadioField("17. 물품 공유 여부", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
-    bug_catch=RadioField("18. 벌레 잡을 수 있는 지 여부", choices=[('Y','그렇다'),('N','아니다')], validators=[InputRequired()])
-    
-    phone_chat_in=RadioField("19-1. 전화 통화 여부", choices=[('실내','실내'),('실외','실외')], validators=[InputRequired()])
-    phone_chat_time=RadioField("19-2. 하루 평균 통화 시간", choices=[('드물게','20분 미만'),('보통','20분~1시간'),('자주','1시간 이상')], validators=[InputRequired()])
-        
-    age_range=RadioField("20. 원하는 룸메 나이대", choices=[('또래', '두 살 차이 내'), ('또래 이상', '두 살 차이 이상')])
     submit=SubmitField("Submit")
-
     
 @app.route("/")
 def hello():
@@ -104,19 +94,18 @@ def hello():
 
 @app.route("/servey", methods=["GET", "POST"])
 def servey():
-    email, age, sex, college_of, newbie_in, diff_college_of, personality, weekend_stay, weekday_stay=None, 0, None, None, None, None, None, None, None
-    smoke, alcohol, how_eat, how_eat_in, wake_up, sleep, stay_overnight_test, sleep_habit, clean_period=None, None, None, None, None, None, None, None, None
-    shower_time, shower_timezone, material_share, bug_catch, phone_chat_in, phone_chat_time, age_range=None, None, None, None, None, None, None
+    email, sex, age, dorm_select, college_of, personality, weekend_stay, weekday_stay=None, 0, None, None, None, None, None, None
+    smoke, alcohol, how_eat, how_eat_in, wake_up, sleep, sleep_sensitive, sleep_habit, clean_period=None, None, None, None, None, None, None, None, None
+    shower_timezone=None
     error_msg=""
     form=UserForm(request.form)
     
-    kwargs={'email': email, 'age': age, 'college_of': college_of,' newbie_in': newbie_in, 'diff_college_of': diff_college_of, 'personality': personality,
-            'weekend_stay': weekend_stay, 'smoke': smoke, 'alcohol': alcohol, 'how_eat': how_eat, 'how_eat_in': how_eat_in, 'wake_up': wake_up,
-            'sleep': sleep, 'stay_overnight_test': stay_overnight_test, 'sleep_habit': sleep_habit, 'clean_period': clean_period, 'shower_time': shower_time, 
-            'shower_timezone': shower_timezone, 'material_share': material_share, 'bug_catch': bug_catch, 'phone_chat_in': phone_chat_in, 
-            'phone_chat_time': phone_chat_time, 'age_range': age_range}
+    kwargs={'email': email, 'sex': sex, 'age': age, 'dorm_select': dorm_select, 'college_of': college_of, 'personality': personality,
+            'weekend_stay': weekend_stay, 'weekday_stay': weekday_stay, 'smoke': smoke, 'alcohol': alcohol, 'how_eat': how_eat, 'how_eat_in': how_eat_in, 'wake_up': wake_up,
+            'sleep': sleep, 'sleep_sensitive': sleep_sensitive, 'sleep_habit': sleep_habit, 'clean_period': clean_period, 
+            'shower_timezone': shower_timezone}
     
-    #Validate Form
+    #Validate Form 입력 완료 후 받은 데이터 저장
     if request.method=="POST" and form.validate():
         print(form.email.data)
         before_mail=form.email.data
@@ -128,33 +117,69 @@ def servey():
             form=UserForm(formdata=None)
             error_msg="중복되는 이메일"
             return render_template("servey.html", error_msg=error_msg, **kwargs, form=form)
-        else:
-            user=Usertbl(email=form.email.data, age=form.age.data, sex=form.sex.data, college_of=form.college_of.data, 
-                         newbie_in=form.newbie_in.data, diff_college_of=form.diff_college_of.data, personality=form.personality.data, 
-                         weekend_stay=form.weekend_stay.data, weekday_stay="&".join(form.weekday_stay.data), smoke=form.smoke.data, alcohol=form.alcohol.data, 
-                         how_eat="&".join(form.how_eat.data), how_eat_in=form.how_eat_in.data, wake_up=form.wake_up.data, sleep=form.sleep.data, 
-                         stay_overnight_test=form.stay_overnight_test.data, sleep_habit=form.sleep_habit.data, clean_period=form.clean_period.data, 
-                         shower_time=form.shower_time.data, shower_timezone=form.shower_timezone.data, material_share=form.material_share.data, 
-                         bug_catch=form.bug_catch.data, phone_chat_in=form.phone_chat_in.data, phone_chat_time=form.phone_chat_time.data, 
-                         age_range=form.age_range.data)
+        else:   # 데이터 입력 되는 부분
+            user=Usertbl(email=form.email.data, sex=form.sex.data, age=form.age.data, dorm_select = form.dorm_select.data ,
+                        college_of=form.college_of.data, personality = form.personality.data, weekend_stay=form.weekend_stay.data, 
+                        weekday_stay=form.weekday_stay.data, smoke=form.smoke.data, alcohol=form.alcohol.data, how_eat=form.how_eat.data, 
+                        how_eat_in=form.how_eat_in.data, wake_up=form.wake_up.data, sleep=form.sleep.data, sleep_sensitive=form.sleep_sensitive.data,
+                        sleep_habit=form.sleep_habit.data, clean_period=form.clean_period.data, shower_timezone=form.shower_timezone.data)
             
             db.session.add(user)
             db.session.commit()
-            return redirect(url_for('qna', email=form.email.data))
+            #my_dic = {''}
+            return redirect(url_for('servey2'))
         
         #Clear Form
         form=UserForm(formdata=None)
     
     return render_template("servey.html", error_msg=error_msg, **kwargs, form=form)
 
+
+@app.route("/servey2", methods=["GET", "POST"])
+def servey2():
+    age_range, diff_college_of, personality, weekend_stay, weekday_stay=None, None, None, None, None
+    smoke, alcohol, how_eat, how_eat_in, wake_up, sleep, sleep_sensitive, sleep_habit, clean_period=None, None, None, None, None, None, None, None, None
+    shower_time, shower_timezone, material_share, bug_catch, phone_chat_in, phone_chat_time, age_range=None, None, None, None, None, None, None
+    dorm_select=None
+    error_msg=""
+    form=MateForm(request.form)
+    
+    kwargs={'age_range': age_range, 'diff_college_of': diff_college_of, 'personality': personality, 'weekend_stay': weekend_stay, 
+            'weekday_stay': weekday_stay, 'smoke': smoke, 'alcohol': alcohol, 'how_eat': how_eat, 'how_eat_in': how_eat_in, 'wake_up': wake_up,
+            'sleep': sleep, 'sleep_sensitive': sleep_sensitive, 'sleep_habit': sleep_habit, 'clean_period': clean_period, 
+            'shower_timezone': shower_timezone}
+    
+    #Validate Form
+    if request.method=="POST":
+        print('성공적1')
+        if form.validate():
+            print('성공적')
+            user_dict={'age_range': form.age_range.data, 'diff_college_of': form.diff_college_of.data, 'personality': form.personality.data, 
+                       'weekend_stay': form.weekend_stay.data, 'weekday_stay': form.weekday_stay.data, 'smoke': form.smoke.data, 'alcohol': form.alcohol.data, 
+                       'how_eat': form.how_eat.data, 'how_eat_in': form.how_eat_in.data, 'wake_up': form.wake_up.data, 'sleep': form.sleep.data, 
+                       'sleep_sensitive': form.sleep_sensitive.data, 'sleep_habit': form.sleep_habit.data, 'clean_period': form.clean_period.data, 
+                       'shower_timezone': form.shower_timezone.data }
+
+
+            return redirect(url_for('qna', user_dict=user_dict), code=307)
+        
+    #Clear Form
+    print('실패') 
+    print(form.errors)
+    form=MateForm(formdata=None)
+    
+    return render_template("servey2.html", error_msg=error_msg, **kwargs, form=form)
+
+
 @app.route("/result")
 def result():
     return render_template("result.html")
 
-@app.route("/qna/<email>", methods=["GET", "POST"])
-def qna(email):
+@app.route("/qna<user_dict>", methods=["GET", "POST"])
+def qna(user_dict):
+    #mate_info=request.form.get("user_dict")
     show_users=Usertbl.query.all()
-    return render_template("qna.html", email=email, show_users=show_users)
+    return render_template("qna.html", user_dict=user_dict, show_users=show_users)
 
 @app.route("/creator")
 def creator():
--- introduce/create_db.py
+++ introduce/create_db.py
@@ -1,7 +1,7 @@
 import pymysql
 
 #MySQL 접속
-my_db=pymysql.connect(host='localhost', user='xxxxxx', passwd='xxxxxxx', db='users', charset='utf8')
+my_db=pymysql.connect(host='localhost', user='joonmo', passwd='os2window', db='users', charset='utf8')
 print(my_db)
 
 #userDB 이용
@@ -11,13 +11,58 @@ cursor.execute('USE users;')
 #INSERT문
 #cursor.execute('INSERT INTO usertbl (email, age, sex, college_of, how_eat) VALUES ("konkuk@konkuk.ac.kr", 22, "M", "경영대", "기숙사 식당");')
 
-#SELECT문
-cursor=my_db.cursor(pymysql.cursors.DictCursor)
-
-cursor.execute('SELECT * FROM usertbl;')
-value=cursor.fetchall()
-print(value)
-
-#MySQL 접속 종료
+#temp
+# email=db.Column(db.String(30), primary_key=True)
+#     sex=db.Column(db.String(5), nullable=False)
+#     age=db.Column(db.Integer, nullable=False)
+#     dorm_select=db.Column(db.String(10), nullable=False)
+    
+#     college_of=db.Column(db.String(30), nullable=False)
+#     personality=db.Column(db.String(5), nullable=False)
+#     weekend_stay=db.Column(db.String(5), nullable=False)
+#     weekday_stay=db.Column(db.String(30), nullable=False)
+#     smoke=db.Column(db.String(5), nullable=False)
+#     alcohol=db.Column(db.String(5), nullable=False)
+#     how_eat=db.Column(db.String(45), nullable=False)
+#     how_eat_in=db.Column(db.String(5), nullable=False)
+#     wake_up=db.Column(db.String(10), nullable=False)
+#     sleep=db.Column(db.String(5), nullable=False)
+#     sleep_sensitive=db.Column(db.String(10), nullable=False)
+#     sleep_habit=db.Column(db.String(5), nullable=False)
+#     clean_period=db.Column(db.String(10), nullable=False)
+#     shower_timezone=db.Column(db.String(5), nullable=False)
+sql="""
+    CREATE TABLE usertbl(
+    email VARCHAR(30) PRIMARY KEY,
+    sex VARCHAR(5) NOT NULL,
+    age INT NOT NULL,
+    dorm_select VARCHAR(10) NOT NULL,
+    college_of VARCHAR(30) NOT NULL,
+    personality VARCHAR(5) NOT NULL,
+    weekend_stay VARCHAR(5) NOT NULL,
+    weekday_stay VARCHAR(30) NOT NULL,
+    smoke VARCHAR(5) NOT NULL,
+    alcohol VARCHAR(5) NOT NULL,
+    how_eat VARCHAR(45) NOT NULL,
+    how_eat_in VARCHAR(5) NOT NULL,
+    wake_up VARCHAR(10) NOT NULL,
+    sleep VARCHAR(5) NOT NULL,
+    sleep_sensitive VARCHAR(10) NOT NULL,
+    sleep_habit VARCHAR(5) NOT NULL,
+    clean_period VARCHAR(10) NOT NULL,
+    shower_timezone VARCHAR(5) NOT NULL
+    );
+"""
+cursor.execute(sql)
 my_db.commit()
 my_db.close()
+# #SELECT문
+# cursor=my_db.cursor(pymysql.cursors.DictCursor)
+
+# cursor.execute('SELECT * FROM usertbl;')
+# value=cursor.fetchall()
+# print(value)
+
+# #MySQL 접속 종료
+# my_db.commit()
+# my_db.close()
